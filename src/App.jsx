3a3d09e40b19c6bff1c9496e417017aada3ecdf0
gowridevi21 <gowrichandu1621@gmail.com>
2026-05-19 10:42:08 -0400

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
