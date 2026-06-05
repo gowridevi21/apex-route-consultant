d25b80e9f45256abb4d5f69b8fc31795c8d0c4b1
gowridevi21 <gowrichandu1621@gmail.com>
2026-06-05 18:28:10 -0400

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBjE-_d-UsuRp_wt2hfl6iDUsijZ0KgrtY",
   authDomain: "apex-route-website-ee028.firebaseapp.com",
@@ -19,3 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
