d6b5e0d0097c3d4fb418ec663fb305a8c235b29c
gowridevi21 <gowrichandu1621@gmail.com>
2026-06-06 16:31:54 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Dashboard from "./pages/Dashboard";
 import LoadAnalyzerExport from "./pages/LoadAnalyzerExport";
 import { useLocation } from "react-router";
 import Loadbalancer from "./pages/Loadbalancer";
-import MyVault from "./pages/MyVault";
+import Myvault from "./pages/Myvault";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -54,7 +54,7 @@ const hideLayout =
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/load-analyzer-export" element={<LoadAnalyzerExport />} />
         <Route path="/load-analyzer" element={<Loadbalancer />} />
-        <Route path="/my-vault" element={<MyVault />} />
+        <Route path="/my-vault" element={<Myvault />} />
       </Routes>
 
       <Footer />
